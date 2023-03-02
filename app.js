236cea9ee7ffa675b606fa52b269a5ff2d709a3a
Isaac Duff <4mustangid@gmail.com>
2023-03-02 16:05:32 -0700

    Finished Levels mostly

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ const heroes = [
         name: 'Slate Slabrock',
         type: 'Wizard',
         damage: 5,
-        health: 100
+        health: 100,
+        level: 1
     },
     {
         name: 'Flint Ironstag',
         type: 'Knight',
         damage: 10,
-        health: 50
+        health: 50,
+        level: 1
     }
 ]
 
@@ -19,7 +21,8 @@ const heroes = [
 const boss = {
     health: 100,
     maxHealth: 100,
-    damage: 25,// this dmg is 5
+    damage: 5,
+    Gold: 1000,
     level: 1
 }
 
@@ -46,15 +49,44 @@ function hurtBoss(damage) {
 
     }
     if (boss.health < 0) boss.health = 0
-    // console.log(boss)
+    if (boss.health == 0) {
+        bossLevelUp()
+        playerLevelUp()
+    }
+    console.log(boss)
 }
 
+
+function bossLevelUp() {
+    boss.maxHealth += 100
+    boss.health = boss.maxHealth
+    boss.damage += 10
+    boss.level++
+    boss.gold = (boss.gold * 2)
+    console.log(boss)
+}
+
+function playerLevelUp() {
+    heroes.forEach(p => {
+        if (p.level == boss.level / 2) {
+            heroes.forEach(p => p.health += 50)
+            heroes.forEach(p => p.damage += 5)
+            heroes.forEach(p => p.level++)
+        }
+    })
+
+    console.log(heroes)
+}
+
+
+//REVIEW - how to make boss stop attacking dead guy
 function attackPlayers() {
     let target = Math.floor(Math.random() * heroes.length)
     let hurtHero = heroes[target]
     hurtHero.health -= boss.damage
+
     if (heroes.every(hero => hero.health <= 0)) {
-        window.prompt("You Suuuuuuuuuuck!!! Get good kid")
+        window.alert("You Suuuuuuuuuuck!!! Get good kid")
     }
 
 
@@ -62,7 +94,7 @@ function attackPlayers() {
     // console.log(target)
 }
 //REVIEW - uncomment this you foooooooool
-// setInterval(attackPlayers, 5000)
+// setInterval(attackPlayers, 2000)
 
 
 
